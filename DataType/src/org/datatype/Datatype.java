e4ec097a05d1d67296fbecf31f82ac4a3eb37276
Princy2802 <princyantony2802@gmail.com>
2026-03-15 08:58:22 +0530

    first commit

---
+++ DataType/src/org/datatype/Datatype.java
@@ -0,0 +1,46 @@
+package org.datatype;
+
+public class Datatype {
+public static void main(String[] args) {
+	//dataType variableName = value;
+	//Student name
+	String name = "Princy";
+System.out.println("my name is "+name);	
+//initial
+char initial ='p';
+System.out.println("intial is"+initial);
+//id
+//age 
+byte age = 34;
+System.out.println("my age is"+age);
+	//id
+int id = 6739867;
+System.out.println("my id is"+id);
+//fees
+short fees = 20000;
+System.out.println("my fees is"+fees);
+//mobile no
+long mobileNo = 9176479966l;
+System.out.println("my no is"+mobileNo);
+//percentage
+float percentage = 89.98f;
+System.out.println("my percentageis"+percentage);
+//overall percent
+double overallPercent = 89.98775454;
+System.out.println("my overall percentis"+overallPercent);
+	
+//status
+boolean staus = false;
+System.out.println("staus is"+staus);
+
+	
+	
+	
+	
+	
+	
+	
+}
+	
+
+}
